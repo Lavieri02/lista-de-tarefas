7ebfb75f02b3a53aa89aca62cf040642951703c0
“Breno <brenolavieri@gmail.com>
2024-09-23 15:02:32 -0300

    Remoção de logs de teste no arquivo script

--- target/classes/static/js/script.js
+++ target/classes/static/js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("Editar tarefa ID: " );
-    // Aqui você pode fazer uma requisição AJAX para carregar dados da tarefa no modal quando for editar
+    
+    
     var editarBtns = document.querySelectorAll('.editarBtn');
     editarBtns.forEach(function (btn) {
         btn.addEventListener('click', function () {
             var tarefaId = this.getAttribute('data-id');
-            // Chamada AJAX para buscar os dados da tarefa a ser editada e preencher no formulário do modal
+            
             console.log("Editar tarefa ID: " + tarefaId);
-            // Você pode adicionar lógica AJAX aqui para carregar os dados no modal antes de exibi-lo
+            
         });
     });
     document.getElementById('formTarefa').addEventListener('submit', function(event) {
